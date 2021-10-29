c2bb00e270a1e82125846d47179474444551fdfa
Juan José Londoño David <juanjoselondonodavid@gmail.com>
2021-10-28 23:46:02 -0500

    VueJS routing Template Loaded

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#">Juan José Londoño David</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <router-link to="/"><b-nav-item href="#" active>Home</b-nav-item></router-link>
+        <router-link to="/about"><b-nav-item href="#">Projects</b-nav-item></router-link>
+        <b-nav-item href="#">Music</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown text="Language" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import { BootstrapVue } from 'bootstrap-vue'
+import VueRouter from 'vue-router';
+import router from './router/index'
+Vue.use(VueRouter);
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  el: '#app',
+  render: h => h(App)
+})
